9d0ccbb6005e8999a1d1f1603c88198e9b683839
Sampad530 <sampad.banik530@gmail.com>
2021-07-25 04:53:05 +0600

    2nd commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,6 @@ package sample;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Marvel World");
+        System.out.println("Hello MCU World");
     }
 }
